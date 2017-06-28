5fdb19a09298747254a6eee8509a3dd4fff1ade5
Doug <doug.steinberg@gmail.com>
2017-06-28 16:29:30 -0400

    Added index page

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,20 +2,32 @@ class PokemonController < ApplicationController
 
   def index
 
+        res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon", followlocation: true)
+        body = JSON.parse(res.body)
+        # body["name"] # should be "pikachu"
+        render json:
+          body["results"].map do |pokemon|
+            {
+              "url": pokemon["url"],
+              "name": pokemon["name"]
+            }
+           end
+          
   end
-
 
   def show
 
     res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}", followlocation: true)
     body = JSON.parse(res.body)
-    # body["name"] # should be "pikachu"
 
-    render json: #body["name"]
+    render json:
       {
           "id":body["id"],
           "name":body["name"],
-          "types":body["types"][0]["type"]["name"]
+          "types":
+           body["types"].map do |type_hash|
+             type_hash["type"]["name"]
+           end
       }
   end
 
